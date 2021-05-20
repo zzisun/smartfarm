24632a731d0aefcf66cb1e8b117a61f55bcc1456
lsmin1021 <dltmdals826@gmail.com>
2021-05-20 22:35:20 +0900

    Add Category

--- product/admin.py
+++ product/admin.py
@@ -2,8 +2,12 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin
-from .models import Product
+from .models import Product, Category
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price', 'stock','image')
+    list_display = ('name', 'price', 'stock','category')
 admin.site.register(Product, ProductAdmin)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('sort',)
+admin.site.register(Category, CategoryAdmin)
--- product/models.py
+++ product/models.py
@@ -9,10 +9,17 @@ from django.db import models
 from django.utils import timezone
 
 
+class Category(models.Model):
+    sort = models.CharField(max_length=255, verbose_name="Category", default='')
+
+    def __str__(self):
+        return '{}'.format(self.sort)
+
 class Product(models.Model):
     #author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     name = models.CharField(max_length=50, verbose_name="Product name", default='')
     #text = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='Category')
     price = models.IntegerField(verbose_name="Price", default=0)
     description = models.TextField(verbose_name="Description", default='')
     image = models.ImageField(upload_to='images/', null = True)
