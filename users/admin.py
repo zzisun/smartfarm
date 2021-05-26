b2b4243c3e14479f2f2054fe6d984f2503cb010e
Ji-Sun Kim <jisunkim98@gmail.com>
2021-05-26 19:34:04 +0900

    Feat: Profile 삭제

--- users/admin.py
+++ users/admin.py
@@ -1,13 +1,11 @@
 from django.contrib import admin
-
+from django.contrib.auth.models import Group
 # Register your models here.
 from django.contrib import admin
 from .models import Users
-from .models import Profile
-
-admin.site.register(Profile)
 
 class UsersAdmin(admin.ModelAdmin):
     list_display = ('email','name')
 
 admin.site.register(Users, UsersAdmin)
+admin.site.unregister(Group)
--- users/models.py
+++ users/models.py
@@ -1,21 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    phone_number = models.TextField(max_length=12, blank=True)
-
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
-
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    instance.profile.save()
-
 
 class Users(models.Model):
     email = models.CharField(verbose_name="email", max_length = 128)
