b896921a9e0e808037b64d19fc8bdfc6bbb41117
Ji-Sun Kim <jisunkim98@gmail.com>
2021-06-01 16:36:03 +0900

    twitter 기능 주석처리

--- users/urls.py
+++ users/urls.py
@@ -2,10 +2,8 @@ from django.contrib import admin
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
-from users.views import userAPI, TwitterAuthRedirectEndpoint, TwitterCallbackEndpoint, TwitterShare
+# from users.views import userAPI, TwitterAuthRedirectEndpoint, TwitterCallbackEndpoint, TwitterShare
 
-from users.views import home
-from users.views import LoginView, RegisterView, userAPI, TwitterAuthRedirectEndpoint, TwitterCallbackEndpoint
 urlpatterns = [
     path('', views.home, name='home'),
     path('mypage/', views.mypage, name='mypage'),
@@ -18,14 +16,14 @@ urlpatterns = [
 
     path('resetPassword/', views.resetPassword, name='resetPassword'),
     path('createPassword/', views.createPassword, name='createPassword'),
-    path('userlist/',userAPI),
+    # path('userlist/',userAPI),
 
     path('password_reset/', auth_views.PasswordResetView.as_view(), name="password_reset"),
     path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
     path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
     path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
 
-    path('userlist/',userAPI),
-    path("auth/twitter/redirect/", TwitterAuthRedirectEndpoint.as_view(), name="twitter-login-redirect"),
-    path("callback/twitter/", TwitterCallbackEndpoint.as_view(), name="twitter-login-callback"),
+    # path('userlist/',userAPI),
+    # path("auth/twitter/redirect/", TwitterAuthRedirectEndpoint.as_view(), name="twitter-login-redirect"),
+    # path("callback/twitter/", TwitterCallbackEndpoint.as_view(), name="twitter-login-callback"),
 ]
