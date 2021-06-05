672cbe89d06e254dfc9c907f0541a12cd18ec638
druwa <wbsl0427@gmail.com>
2021-06-05 19:26:12 +0900

    add farm list page and device4, mypage

--- device_management/urls.py
+++ device_management/urls.py
@@ -22,6 +22,7 @@ app_name = "device_management"
 
 urlpatterns = [
     path('', views.device1, name='device1'),
+    path('mypage',views.mypage, name='mypage'),
     path('control',views.control, name='control'),
     path('device',views.device, name='device'),
     path('device1', views.device1, name='device1'),
@@ -33,4 +34,5 @@ urlpatterns = [
     path('create_farm_info', views.create_farm_info.as_view(), name='Create_farm_info'),
     path('crop_info_registeration', views.crop_info_registeration.as_view(), name='crop_info_registeration'),
     path('status', views.status, name='status'),
+
 ]
--- device_management/views.py
+++ device_management/views.py
@@ -5,6 +5,7 @@ from .models import Device_Info, Farm_Info, Growth_Params, Plant_Info
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import POST_Growth_Param_Serializer, POST_Mock, POST_Farm_Info, POST_Plant_Info
+from users.models import Users
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
@@ -12,23 +13,23 @@ def index(request):
 def control(request):
     return render(request, 'device_management/control.html')
 
+def mypage(request):
+    user = request.user
+    device_info = Device_Info.objects.filter(device_user=user)[0]
+    print(device_info)
+    farm_list = Farm_Info.objects.filter(device_info=device_info)
+    print(farm_list)
+    return render(request, 'device_management/mypage.html', {'farm_list': farm_list})
 
 # has to fix when Device data is exist, move to control.html, not exists, move to device1.html
 # In this time, just get data from html3, device_serial, device_name.
 def device(request):
-    device_name = request.GET.get("device_name")
-    device_serial = request.GET.get("device_serial")
-    device_model = request.GET.get("modelno")
-    device_detail = request.GET.get("detail")
-    device_ip_address = request.GET.get("ip_address")
-    device_password = request.GET.get("password")
-
-    print("name=id =" + device_name, device_serial, device_ip_address, "password : "+ device_password)
     device_information = Device_Info.objects.all()
-    
-
+    print(request.user)
+    print(device_information)
     try :
         device_information = Device_Info.objects.create(
+            device_user = request.user,
             device_serial = request.GET.get("device_serial"), \
             device_detail = request.GET.get("detail"), \
             device_model = request.GET.get("modelno"), \
@@ -46,15 +47,19 @@ def device1(request):
     return render(request, 'device_management/device1.html')
 
 def device2(request):
+
+
     return render(request, 'device_management/device2.html')
 
 def device3(request):
     return render(request, 'device_management/device3.html')
 
 def device4(request):
-    
-    
-    return render(request, 'device_management/device4.html')
+    try:
+        device_info = Device_Info.objects.filter(device_user=request.user)[0]
+        return render(request, 'device_management/device4.html', {'device_info': device_info})
+    except:
+        return redirect('device_management:device1')
 
 
 
