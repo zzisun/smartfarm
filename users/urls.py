fd9388423315e9ac97d2c76bb879875972b3773b
Ji-Sun Kim <jisunkim98@gmail.com>
2021-06-07 15:49:03 +0900

    password_reset 템플릿 변경

--- users/urls.py
+++ users/urls.py
@@ -18,10 +18,21 @@ urlpatterns = [
     path('resetPassword/', views.resetPassword, name='resetPassword'),
     path('createPassword/', views.createPassword, name='createPassword'),
 
-    path('password_reset/', auth_views.PasswordResetView.as_view(), name="password_reset"),
-    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
-    path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
-    path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
+    path('reset_password/',
+     auth_views.PasswordResetView.as_view(template_name="users/password_reset.html"),
+     name="reset_password"),
+
+    path('reset_password_sent/', 
+        auth_views.PasswordResetDoneView.as_view(template_name="users/password_reset_sent.html"), 
+        name="password_reset_done"),
+
+    path('reset/<uidb64>/<token>/',
+     auth_views.PasswordResetConfirmView.as_view(template_name="users/password_reset_form.html"), 
+     name="password_reset_confirm"),
+
+    path('reset_password_complete/', 
+        auth_views.PasswordResetCompleteView.as_view(template_name="users/password_reset_done.html"), 
+        name="password_reset_complete"),
 
     path('userlist/',userAPI),
     path("auth/twitter/redirect/", TwitterAuthRedirectEndpoint.as_view(), name="twitter-login-redirect"),
